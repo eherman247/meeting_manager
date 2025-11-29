85788bd8806fbb1058260c925ba9b25bfee68ba1
Ethan <eherman247@gmail.com>
2025-11-28 22:22:07 -0700

    more user authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,10 @@
 const User = require('../models/userModel')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, {expiresIn: '1d'})
+}
 
 // get a single user
 const loginUser = async (req, res) => {
@@ -21,7 +25,9 @@ const createAccount = async (req, res) => {
   const {fname, lname, email, password} = req.body
   try {
     const user = await User.signup(fname, lname, email, password)
-    res.status(200).json(user)
+
+    const token = createToken(user._id)
+    res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 const userScheme = new Schema({
@@ -25,8 +26,15 @@ const userScheme = new Schema({
 // signup method to create a new user
 userScheme.statics.signup = async function(fname, lname, email, password) {
   // validation
+
   if (!fname || !lname || !email || !password) {
     throw Error('All fields must be filled')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email is not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password is not strong enough')
   }
   const exists = await this.findOne({email})
   if (exists) {
